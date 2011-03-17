a61411c67c94aee57e06f271d5e12ff34f107b2e
irony9501 <y_imamura@preferred.jp>
2011-03-17 18:54:29 +0900

    bugfix

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,3 +1,11 @@
 class Content < ActiveRecord::Base
   belongs_to :category
+
+  CONTENT_TYPE_RECENT = 1
+  CONTENT_TYPE_TODAY = 2
+  CONTENT_TYPE_YESTERDAY = 3
+  CONTENT_TYPE_WEEK = 4
+
+  
+
 end
