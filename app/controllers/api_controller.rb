3810989f8516a7ae78abf263ba7117116ed89ed4
irony9501 <y_imamura@preferred.jp>
2011-03-18 14:19:39 +0900

    get_contentの処理実態をContentモデルに委譲

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,29 +3,11 @@ require 'json'
 
 class ApiController < ApplicationController
   def get_contents
-    cid = params[:cid]
-    type = params[:type].to_i
-    cat = Category.find_by_id(cid.to_i)
-    content = Content.find(:first, :conditions => {:content_type => type, :category_id => cid})
-    (render(:json => ["resource not found"], :status => :not_found ) and return) unless cat && type && content
-    ret = {:pages => [], :type => type, :category_name => cat.name}
-
-    pagesjson = JSON.parse(content.json)
-    pagesjson.each do |p|
-      page = Page.find_by_url(p[1])
-      if page && !page.spam
-        tmp = {
-          :count => p[0],
-          :url => p[1],
-          :id => page.id,
-          :title => page.title || "",
-          :description => page.description || "",
-          :image_url => page.image_url || "",
-        }
-        ret[:pages].push(tmp)
-      end
+    ret = Content.get_contents(params[:cid].to_i, params[:type].to_i)
+    if ret.empty?
+      render(:json => ["resource not found"], :status => :not_found )
+    else
+      render :json => ret, :callback => params[:callback]
     end
-
-    render :json => ret, :callback => params[:callback]
   end
 end
--- app/models/content.rb
+++ app/models/content.rb
@@ -6,6 +6,30 @@ class Content < ActiveRecord::Base
   CONTENT_TYPE_YESTERDAY = 3
   CONTENT_TYPE_WEEK = 4
 
+  def self.get_contents(catid, ctype)
+    cat = Category.find_by_id(catid)
+    content = Content.find(:first, :conditions => {:content_type => ctype, :category_id => catid})
+    return {} unless cat && ctype && content
+    ret = {:pages => [], :type => ctype, :category_name => cat.name}
+
+    pagesjson = JSON.parse(content.json)
+    pages = Page.find(:all, :conditions => {:spam => false, :url => pagesjson.map{|a| a[1]}})
+    pagesjson.each do |p|
+      page = pages.select{|a| a.url == p[1]}[0]
+      if page
+        tmp = {
+          :count => p[0],
+          :url => p[1],
+          :id => page.id,
+          :title => page.title || "",
+          :description => page.description || "",
+          :image_url => page.image_url || "",
+        }
+        ret[:pages].push(tmp)
+      end
+    end
+    return ret
+  end
   
 
 end
