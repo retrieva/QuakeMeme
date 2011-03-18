a0eeb436492bd1e8ff7ffb38b428ef4ba7461706
Kazuki Ohta <kazuki.ohta@gmail.com>
2011-03-18 15:26:23 +0900

    added caching to ApiController

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,11 +3,35 @@ require 'json'
 
 class ApiController < ApplicationController
   def get_contents
-    ret = Content.get_contents(params[:cid].to_i, params[:type].to_i)
-    if ret.empty?
+    # read from cache
+    cache_key = "qm_api_cache_#{params[:cid]}_#{params[:type]}"
+    begin
+      cache_val = Rails.cache.read(cache_key)
+    rescue MemCacheError
+      cache_val = nil
+    rescue
+      cache_val = nil
+    end
+
+    # check cache-miss or memcached is down
+    if cache_val.nil?
+      json = Content.get_contents(params[:cid].to_i, params[:type].to_i)
+    else
+      json = cache_val
+    end
+
+    # write to cache with expiration
+    begin
+      Rails.cache.write(cache_key, json, :expires_in => 1.minute) unless (json.nil? or json.empty?)
+    rescue MemCacheError
+    rescue
+    end
+
+    # show
+    if json.empty?
       render(:json => ["resource not found"], :status => :not_found )
     else
-      render :json => ret, :callback => params[:callback]
+      render :json => json, :callback => params[:callback]
     end
   end
 end
