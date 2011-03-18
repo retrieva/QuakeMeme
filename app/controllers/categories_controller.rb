aa3da43abaee2980ad9bec0e0f3078bca4eba6b0
Kei AKITA <ofkjpn+github@gmail.com>
2011-03-18 21:32:10 +0900

    categoryのactive scaffoldにcidを表示できるように

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
   before_filter :require_admin
   
   active_scaffold :category do |config|
+    config.columns << :id
   end
 
   layout "activescaffold"
